61b85eeb58377ea4eed506a8dd95d3fff4ab8ddc
Yashraj Limkar <yashraj1416@gmail.com>
2025-06-23 00:57:20 +0530

    Added Models

---
+++ leads/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+
+    f_name = models.CharField(max_length=20)
+    l_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    # phoned = models.BooleanField(default=False)
+    # source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField()
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+
+
